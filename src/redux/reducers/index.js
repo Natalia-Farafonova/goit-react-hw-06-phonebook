7534d6842b3fd044f8b860c7598bc13b12dd71d6
Наталия Фарафонова <ferymfon1983@gmail.com>
2022-01-18 17:08:20 +0200

    рефакторинг кода с toolkin

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import { contactsReducer, filterReducer } from '../reducers/contacts'; 
+
+
+
+export const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
+});
